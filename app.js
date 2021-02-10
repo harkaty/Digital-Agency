8b086f062a313270276a666b04cf220ea6b45ec2
Abdessittir Harkati <aharkati@atlassian.com>
2021-02-10 12:39:34 +0100

    add new features

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const humberger = document.querySelector('.humberger');
 const navbar1 = document.querySelector('.nav');
+const search = document.querySelector('.searchIcon');
 
 humberger.addEventListener("click", () =>{
     navbar1.classList.toggle('active');
@@ -7,4 +8,7 @@ humberger.addEventListener("click", () =>{
 });
 window.addEventListener('scroll', () =>{
    navbar1.classList.toggle('change');
+});
+search.addEventListener("click", () => {
+    navbar1.classList.toggle('search');
 });
